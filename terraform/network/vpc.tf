2a64a6097f3b9ec6529dbb1281e657d9be5324dd
devbytom <devbytom@gmail.com>
2021-04-13 20:59:30 -0300

    feat(tf/network): add vpc settings

---
+++ terraform/network/vpc.tf
@@ -0,0 +1,36 @@
+resource "google_compute_network" "default" {
+  name        = "default"
+  description = "Default network for the project"
+}
+
+resource "google_compute_firewall" "server" {
+  name    = "default"
+  network = google_compute_network.default.name
+
+  allow {
+    protocol = "icmp"
+  }
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22-5000"]
+  }
+
+}
+
+resource "google_compute_global_address" "private_ip_range" {
+  network       = google_compute_network.default.id
+  name          = "google-managed-services-sql-instance"
+  purpose       = "VPC_PEERING"
+  address_type  = "INTERNAL"
+  address       = "10.0.1.0"
+  prefix_length = 24
+
+  depends_on = [google_compute_network.default]
+}
+
+resource "google_service_networking_connection" "private_vpc_connection" {
+  network                 = google_compute_network.default.name
+  service                 = "servicenetworking.googleapis.com"
+  reserved_peering_ranges = [google_compute_global_address.private_ip_range.name]
+}
