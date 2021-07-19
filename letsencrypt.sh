e4f10bbbfa939111baf76417eafa723a30badd9e
tom <devbytom@gmail.com>
2021-07-19 20:41:27 -0300

    Add certbot `edu.kahlo.cc`

--- letsencrypt.sh
+++ letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(*.kahlo.cc)
+domains=(edu.kahlo.cc)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="devbytom@gmail.com" # Adding a valid address is strongly recommended
